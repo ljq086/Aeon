d4ae1de52ee0401f899bcebb63c064eea4d37d5a
unknown <Igor@PROMETHEUS.(none)>
2012-02-29 11:54:56 +0100

    Initial aeon commit

---
+++ aeon.js
@@ -0,0 +1,38 @@
+window.__time = Number(new Date());
+
+/*# Include: external/jquery.js */
+/*# Include: external/linq.js */
+
+/*!
+ * Aeon JavaScript Library v1.0
+ * http://cycle99.com/projects/aeon
+ *
+ * Copyright 2011, Igor France
+ */
+/*# Include: core/Type.js */
+/*# Include: core/Array.js */
+/*# Include: core/Number.js */
+/*# Include: core/Date.js */
+/*# Include: core/Function.js */
+/*# Include: core/Object.js */
+/*# Include: core/String.js */
+
+/*# Include: utils/Assert.js */
+/*# Include: utils/Cookie.js */
+/*# Include: utils/Event.js */
+/*# Include: utils/Log.js */
+/*# Include: utils/Xml.js */
+/*# Include: utils/Url.js */
+/*# Include: utils/Dom.js */
+/*# Include: utils/Css.js */
+
+/*# Include: Event.js */
+/*# Include: Constants.js */
+/*# Include: Dispatcher.js */
+/*# Include: Initializer.js */
+/*# Include: QueryString.js */
+/*# Include: Settings.js */
+
+/*# Include: controls/ControlTypeInfo.js */
+/*# Include: controls/ControlRegistry.js */
+/*# Include: controls/HtmlControl.js */
